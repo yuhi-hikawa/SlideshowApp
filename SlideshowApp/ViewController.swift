107c74fc873308efd8f27d708ebe554e7cd727ca
樋川雄飛 <hikawayuuhi@hikawayuuhinomacbook-pro.local>
2019-07-11 09:19:05 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 樋川雄飛 on 2019/07/11.
+//  Copyright © 2019 Yuhi Hikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
